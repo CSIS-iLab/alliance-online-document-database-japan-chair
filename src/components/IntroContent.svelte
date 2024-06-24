fdedb7ddf7ffc103ac356e7e8137539bd50fc99b
jdromero88 <jdromerov88@gmail.com>
2024-06-24 11:53:37 -0400

    Update title and overline title

--- src/components/IntroContent.svelte
+++ src/components/IntroContent.svelte
@@ -8,10 +8,10 @@
 <div class="wrapper">
   <main class="container intro-content">
     <p class="intro-content__overline--small">
-      The Evolution of the U.S. - Japan Alliance
+      <!-- The Evolution of the U.S. - Japan Alliance -->
     </p>
-    <p class="intro-content__overline--regular">by the Japan Chair Program</p>
-    <h1 class="intro-content__title">Alliance Online Database</h1>
+    <p class="intro-content__overline--regular">Alliance Online</p>
+    <h1 class="intro-content__title">U.S.-Japan Policy Database</h1>
     <p class="intro-content__introduction">
       A curated database of official documents to help users understand the
       history of the U.S.-Japan Alliance from the post-WWII period to the
