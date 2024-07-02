d543da76a8cd61c9e7c4088e309d4d5c18cdbb58
jdromero88 <jdromerov88@gmail.com>
2024-07-02 11:47:17 -0400

    Fix layout to show title and subtitle and adjust padding of descripton on mobile

--- src/components/IntroContent.svelte
+++ src/components/IntroContent.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <div class="wrapper">
-  <main class="container container--desktop intro-content --desktop">
+  <main class="container intro-content">
     <p class="intro-content__overline--small">
       <!-- The Evolution of the U.S. - Japan Alliance -->
     </p>
-    <p class="intro-content__overline--regular">Alliance Online</p>
-    <h1 class="intro-content__title">U.S.-Japan Policy Database</h1>
-    <p class="intro-content__introduction">
+    <p class="show-on-mobile intro-content__overline--regular">Alliance Online</p>
+    <h1 class="show-on-mobile intro-content__title">U.S.-Japan Policy Database</h1>
+    <p class="desktop intro-content__introduction">
       A curated database of official documents to help users understand the
       history of the U.S.-Japan Alliance from the post-WWII period to the
       present day. This is part of the Alliance Online project, created by the <a
@@ -35,12 +35,12 @@
   </main>
 </div>
 
-<main class="container container--mobile intro-content">
+<main class="container mobile intro-content intro-content--mobile">
   <p class="intro-content__overline--small">
     <!-- The Evolution of the U.S. - Japan Alliance -->
   </p>
-  <p class="intro-content__overline--regular">Alliance Online</p>
-  <h1 class="intro-content__title">U.S.-Japan Policy Database</h1>
+  <!-- <p class="intro-content__overline--regular">Alliance Online</p>
+  <h1 class="intro-content__title">U.S.-Japan Policy Database</h1> -->
   <p class="intro-content__introduction">
     A curated database of official documents to help users understand the
     history of the U.S.-Japan Alliance from the post-WWII period to the
